6dc8346b68ed5a0e4dec1d09839cd59a9346e7fb
Ира Ира <irakante@gmail.com>
2019-01-25 15:15:39 +0200

    Add project files.

---
+++ Kantsevich_weather/Kantsevich_weather/ServicesHandler/WeatherServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Kantsevich_weather.Models;
+using Kantsevich_weather.WeatherRestClient ;
+
+namespace Kantsevich_weather.ServicesHandler
+{
+    public class WeatherServices
+    {
+        OpenWeatherMap<WeatherMainModel> _openWeatherRest = new OpenWeatherMap<WeatherMainModel>();
+        public async Task<WeatherMainModel> GetWeatherDetails(string city)
+        {
+            var getWeatherDetails = await _openWeatherRest.GetAllWeathers(city);
+            return getWeatherDetails;
+        }
+    }
+}
---
+++ Kantsevich_weather/Kantsevich_weather/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Kantsevich_weather.Models;
+using Kantsevich_weather.ServicesHandler;
+using System.Threading.Tasks;
+
+namespace Kantsevich_weather.ViewModels
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        WeatherServices _weatherServices = new WeatherServices();
+
+        private WeatherMainModel _weatherMainModel;  // for xaml binding
+        public WeatherMainModel WeatherMainModel
+        {
+            get { return _weatherMainModel; }
+            set
+            {
+                _weatherMainModel = value;
+                IconImageString = "http://openweathermap.org/img/w/" + _weatherMainModel.weather[0].icon + ".png"; // fetch weather icon image
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _city;   // for entry binding and for method parameter value
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                _city = value;
+                Task.Run(async () => {
+                    await InitializeGetWeatherAsync();
+                });
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _iconImageString; // for weather icon image string binding
+        public string IconImageString
+        {
+            get { return _iconImageString; }
+            set
+            {
+                _iconImageString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isBusy;   // for showing loader when the task is initializing
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private async Task InitializeGetWeatherAsync()
+        {
+            try
+            {
+                IsBusy = true; // set the ui property "IsRunning" to true(loading) in Xaml ActivityIndicator Control
+                WeatherMainModel = await _weatherServices.GetWeatherDetails(_city);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
